4f62a26c67fc97691bc1fe874cee12252dc3d3a4
Jared Brown <jaredbrown1@hotmail.com>
2021-06-25 22:25:41 -0700

    created first type

---
+++ types.ts
@@ -0,0 +1,11 @@
+// number
+// 1, 5.3, -10
+// All numbers, no differentiation between integers or floats
+
+// string
+// "hi"
+// All text values
+
+// boolean
+// true, false
+// just these two, no 'truthy" or "falsy" values
